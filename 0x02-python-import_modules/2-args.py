136f45a67c8a4f0584f937c038adbd437f4ac1a1
EGOH WISDOM <wisdompeter83@gmail.com>
2023-01-12 01:46:15 +0100

    Add files via upload

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    args = len(sys.argv)
-    if(args <= 1):
-        print("{} arguments.".format(args - 1))
-    elif(args == 2):
-        print("{} argument:".format(args - 1))
-    else:
-        print("{} arguments:".format(args - 1))
-    for x in range(1, args):
-        print("{}: {}".format(x, sys.argv[x]))
+import sys
+
+if __name__ == '__main__':
+    """Prints the argument list passed to the program
+
+    The program takes all the arguments starting from the second
+    and prints the number of arguments and their value
+
+    """
+    av = sys.argv
+    l_av = len(av) - 1
+
+    if l_av > 1:
+        print(l_av, 'arguments:')
+        for i in range(1, l_av + 1):
+            print('{:d}: {}'.format(i, av[i]))
+    elif l_av == 1:
+        print(l_av, 'argument:')
+        for i in range(1, l_av + 1):
+            print('{:d}: {}'.format(i, av[i]))
+    elif l_av == 0:
+        print(l_av, 'arguments.')
